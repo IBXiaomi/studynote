05898dac1ab98e478fc2d3ad9cc813cac33573b5
baxixiaomi <675837004@qq.com>
2020-09-01 20:21:48 +0800

    edit the contents about test code

--- src/test/java/com/baxixiaomi/code/demo/mockito/service/StudentServiceTest.java
+++ src/test/java/com/baxixiaomi/code/demo/mockito/service/StudentServiceTest.java
@@ -2,33 +2,35 @@ package com.baxixiaomi.code.demo.mockito.service;
 
 import com.baxixiaomi.code.demo.mockito.dao.StudentDao;
 import com.baxixiaomi.code.demo.mockito.domain.Student;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StudentServiceTest {
 
-    private StudentService studentService;
-
+    @Mock
     private StudentDao studentDao;
 
+    private StudentService studentService;
 
     @Before
-    public void setUp() throws Exception {
-        this.studentDao = Mockito.mock(StudentDao.class);
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
         this.studentService = new StudentService(studentDao);
     }
 
     @Test
     public void getStudent() {
-        //Assert.assertNotNull(student);
-        Mockito.when(studentService.getStudent(1)).thenReturn("success");
-        //Assert.assertEquals("success",studentService.getStudent(1));
+        Student student = new Student(1, "jamie");
+        when(studentDao.fetchStudent(1)).thenReturn(student);
+        boolean jamie = studentService.getStudent(1, "jamie");
+        assertTrue(jamie);
     }
 }
